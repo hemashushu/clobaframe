71a68ba9bd4671f36d58cdeb3e8b18db259b4edb
Hippo Spark <hippospark@gmail.com>
2015-05-28 15:43:10 +0800

    Add search (full text index) module.

---
+++ source/search/src/test/java/org/archboy/clobaframe/UnitTest.java
@@ -0,0 +1,32 @@
+package org.archboy.clobaframe;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import javax.inject.Inject;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author yang
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "/applicationContext.xml" })
+public class UnitTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSend() {
+	}
+
+}
